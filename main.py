e259b894c180b4d884d84c10f98b15511acf5f13
kyle <notxap55@yahoo.com>
2025-10-24 15:13:41 -0700

    starting spotify integration

--- main.py
+++ main.py
@@ -2,23 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 
 BASE_URL = 'https://www.billboard.com/charts/hot-100/'
+CLIENT_ID = '64822ce98bbc45c9a1ada1c233647d24'
+CLIENT_SECRET = '0621f3d75e83426db01f596ef7f13449'
 
-# get_date = input('Which year do you want to travel to?  Type the date in this format YYYY-MM-DD: ')
+# get_date = input('Which date do you want to travel to?  Type the date in this format YYYY-MM-DD: ')
 get_date = '2010-02-02'
 
 header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0"}
-
 response = requests.get(url=f'{BASE_URL}{get_date}', headers=header)
 
 soup = BeautifulSoup(response.text, 'html.parser')
-with open('songs.html', 'w', encoding='utf-8') as f:
-    f.write(soup.prettify())
-
+# with open('songs.html', 'w', encoding='utf-8') as f:
+#     f.write(soup.prettify())
 songs_h3 = soup.select('li ul li h3')
-
 # for song in songs_h3:
 #     print(song.get_text().strip())
-
 song_names = [song.get_text().strip() for song in songs_h3]
-
 print(song_names)
